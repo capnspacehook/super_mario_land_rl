d2c2ead88a248240fad075980c3bcae4ce7ef938
Andrew LeFevre <jalefevre@liberty.edu>
2024-10-27 10:17:12 -0400

    update sweep again

--- sweep.py
+++ sweep.py
@@ -21,12 +21,12 @@ def sweep(args, train):
         ),
         Param(
             name="batch_size",
-            space=LinearSpace(min=15, max=20, scale=5, is_integer=True),
+            space=LinearSpace(min=15, max=17, scale=3, is_integer=True),
             search_center=17,
         ),
         Param(
             name="minibatch_size",
-            space=LinearSpace(min=2, max=32, scale=16, is_integer=True),
+            space=LinearSpace(min=4, max=32, scale=16, is_integer=True),
             search_center=4,
         ),
         Param(
@@ -34,13 +34,13 @@ def sweep(args, train):
         ),
         Param(name="learning_rate", space=LogSpace(min=1e-5, max=1e-1), search_center=0.0001),
         Param(name="gamma", space=LogitSpace(min=0.9, max=0.9999), search_center=0.995),
-        Param(name="gae_lambda", space=LogitSpace(min=0.8, max=1.0), search_center=0.98),
+        Param(name="gae_lambda", space=LogitSpace(min=0.5, max=1.0), search_center=0.98),
         Param(name="ent_coef", space=LogSpace(min=1e-5, max=1e-1), search_center=0.0075),
         Param(name="vf_coef", space=LogitSpace(min=0.0, max=1.0), search_center=0.3),
         Param(name="max_grad_norm", space=LinearSpace(min=0, max=5), search_center=1),
         Param(
             name="bptt_horizon",
-            space=LinearSpace(min=8, max=64, scale=32, is_integer=True),
+            space=LinearSpace(min=2, max=128, scale=32, is_integer=True),
             search_center=16,
         ),
     ]
@@ -77,43 +77,52 @@ def trainWithSuggestion(args, params, train):
     wandb.init()
     args.track = False
 
-    config = CARBSParams(
-        better_direction_sign=1,
-        max_suggestion_cost=21600,  # 6h
-        num_random_samples=len(params),
-        is_wandb_logging_enabled=False,
-    )
-    carbs = CARBS(config=config, params=params)
-
-    wandbCarbs = WandbCarbs(carbs=carbs)
-    print(
-        f"Initialized CARBS with "
-        + json.dumps(
-            {
-                "observations": wandbCarbs._num_observations,
-                "failures": wandbCarbs._num_failures,
-                "running": wandbCarbs._num_running,
-                "defunct": wandbCarbs._defunct,
-            }
-        )
-    )
-    print(f"CARBS is random sampling: {carbs._is_random_sampling()}")
-    args.wandb = wandb
-
-    suggestion = wandbCarbs.suggest()
-    del suggestion["suggestion_uuid"]
-    args.train.__dict__.update(dict(suggestion))
-
-    args.train.batch_size = 2**args.train.batch_size
-    args.train.minibatch_size = closest_power(args.train.minibatch_size)
-    args.train.minibatch_size = args.train.batch_size // args.train.minibatch_size
-    args.train.bptt_horizon = closest_power(args.train.bptt_horizon)
-    wandb.run.config.update(dict(args.train), allow_val_change=True)
-
-    print(f"Training args: {wandb.run.config}")
-
-    startTime = time.time()
     try:
+        config = CARBSParams(
+            seed=int(time.time()),
+            better_direction_sign=1,
+            max_suggestion_cost=21600,  # 6h
+            num_random_samples=len(params),
+            is_wandb_logging_enabled=False,
+        )
+        carbs = CARBS(config=config, params=params)
+
+        wandbCarbs = WandbCarbs(carbs=carbs)
+        print(
+            f"Initialized CARBS with "
+            + json.dumps(
+                {
+                    "observations": wandbCarbs._num_observations,
+                    "failures": wandbCarbs._num_failures,
+                    "running": wandbCarbs._num_running,
+                    "defunct": wandbCarbs._defunct,
+                }
+            )
+        )
+        print(f"CARBS is random sampling: {carbs._is_random_sampling()}")
+        args.wandb = wandb
+
+        suggestion = wandbCarbs.suggest()
+        del suggestion["suggestion_uuid"]
+        print(f"Suggestion: {suggestion}")
+        args.train.__dict__.update(dict(suggestion))
+
+        args.train.batch_size = 2**args.train.batch_size
+        args.train.minibatch_size = closest_power(args.train.minibatch_size)
+        args.train.minibatch_size = args.train.batch_size // args.train.minibatch_size
+        args.train.bptt_horizon = closest_power(args.train.bptt_horizon)
+        wandb.run.config.update(
+            dict(
+                batch_size=args.train.batch_size,
+                minibatch_size=args.train.minibatch_size,
+                bptt_horizon=args.train.bptt_horizon,
+            ),
+            allow_val_change=True,
+        )
+
+        print(f"Training args: {wandb.run.config}")
+
+        startTime = time.time()
         evalInfos = train(args)
     except Exception:
         Console.print_exception()
